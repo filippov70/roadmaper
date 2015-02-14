17cc2b509ece1e376904e63ce0fbf8b711dfec3c
malig <malig33@mail.ru>
2015-02-14 13:02:49 +0600

    refactoring public

---
+++ public/js/configuration.js
@@ -0,0 +1,19 @@
+﻿define([], function () {
+
+	return {
+
+		defaultImg: '/img/none.jpg',
+		
+		errorImg: '/img/error.png',
+
+		dirImg: '/img/',
+
+		productsImg: '/img/products/',
+
+		categoriesImg: '/img/category/',
+
+		subcategoriesImg: '/img/subcategory/'
+
+	};
+
+});
---
+++ public/js/require-config.js
@@ -0,0 +1,38 @@
+﻿requirejs.config({
+
+	baseUrl: '/js/',
+
+	paths: {
+
+		domReady: 'libs/domReady-2.0.1',
+
+		jquery: 'libs/jquery/jquery-1.11.1.min',	
+
+		autocomplete: 'libs/jquery.devbridge-autocomplete/jquery.devbridge-autocomplete.min',
+
+		ol: 'libs/ol3/ol',
+
+		bootstrap: 'libs/twitter-bootstrap/js/bootstrap',
+
+		conf: 'configuration'
+
+	},
+
+	shim: {
+
+		'autocomplete': {
+			deps:['jquery']
+		},
+
+		'ol': {
+			deps:['jquery']
+		},		
+
+		'bootstrap': {
+			deps:['jquery']
+		}
+		
+	},	
+
+	waitSeconds: 200
+});
