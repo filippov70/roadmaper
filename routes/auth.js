bb78abe0093c5bee6a5b0e18b133a0e4bc9f8a6e
malig <malig33@mail.ru>
2015-02-23 16:36:59 +0600

    auth form

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,16 @@ var passport = require('passport');
 
 module.exports = function (app) {
 
+    app.get('/map', function (req, res) {
+
+        if (req.isAuthenticated()) {
+            res.render('map.ejs');
+            return;
+        }     
+           
+        res.redirect('/auth');
+    });
+
     app.get('/auth', function (req, res) {
 
         if (req.isAuthenticated()) {
@@ -25,33 +35,4 @@ module.exports = function (app) {
         failureFlash: true })
     );
 
-    app.get('/auth/fb',
-        passport.authenticate('facebook', {
-            scope: 'read_stream'
-        })
-    );
-
-    app.get('/auth/fb/callback',
-        passport.authenticate('facebook', {
-            successRedirect: '/',
-            failureRedirect: '/auth' })
-    );
-
-    app.get('/auth/vk',
-        passport.authenticate('vk', {
-            scope: ['friends']
-        }),
-        function (req, res) {
-            // The request will be redirected to vk.com for authentication, so
-            // this function will not be called.
-        });
-
-    app.get('/auth/vk/callback',
-        passport.authenticate('vk', {
-            failureRedirect: '/auth'
-        }),
-        function (req, res) {
-            // Successful authentication, redirect home.
-            res.redirect('/');
-        });
 }
